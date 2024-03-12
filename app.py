869c3f3f6daa4e447d8f009b9812dd39bcabbdd9
Hameed <abdulhameed793.a@gmail.com>
2024-03-12 16:37:02 +0530

    12 march

--- app.py
+++ app.py
@@ -6,21 +6,26 @@ app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']=  'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATION']=False
 db = SQLAlchemy(app)
+app.app_context().push()
 
 N=100000  #time for revoking access automatically
 
 class book(db.Model):
+    __tablename__= 'book'
     ID=db.Column(db.Integer,primary_key=True)
     Name=db.Column(db.String(200),nullable=False,default="no title assigned") 
-    Category=db.Column(db.String(200),default="no category assigned")
+    Category=db.Column(db.String(200),db.ForeignKey('section.Name'),default="no category assigned", )
+    category = db.relationship('section', backref=db.backref('book', lazy=True))
     Author=db.Column(db.String(200),default="no author assigned")
     D_issue=db.Column(db.DateTime,default=datetime.utcnow)
-    Content=db.Column(db.String(1000),default="no content assigned")
+    Content=db.Column(db.String(10000),default="no content assigned")
+    Description=db.Column(db.String(1000),default='nothing here')
     Rating=db.Column(db.Integer,default="no rating assigned")
     def __repr__(self):
         return '<Book %r>' %self.id 
 
 class section(db.Model):
+    __tablename__='section'
     ID=db.Column(db.Integer,primary_key=True)
     Name=db.Column(db.String(200),default="no name assigned")
     D_issue=db.Column(db.DateTime,default=datetime.utcnow)
@@ -28,6 +33,7 @@ class section(db.Model):
     S_books=db.Column(db.Integer)
 
 class users(db.Model):
+    __tablename__='users'
     ID=db.Column(db.Integer,primary_key=True)
     Name=db.Column(db.String(100),default="user")
     Password=db.Column(db.String(15),default="0000")
@@ -35,15 +41,18 @@ class users(db.Model):
     Role=db.Column(db.String(200),default="no role assigned") 
 
 class register(db.Model):
+    __tablename__='register'
     ID=db.Column(db.Integer,primary_key=True)
-    F_book_id=db.Column(db.Integer)
+    F_book_id = db.Column(db.Integer, db.ForeignKey('book.ID'))
+    book_id= db.relationship('book', backref=db.backref('register', lazy=True))
     D_grant=db.Column(db.DateTime,default=datetime.utcnow)
     D_return=db.Column(db.DateTime,default=datetime.utcnow)
-    F_user_id=db.Column(db.String(200))
+    F_user_id = db.Column(db.String(200), db.ForeignKey('users.ID'))
+    user_id= db.relationship('users', backref=db.backref('register', lazy=True))
     Status=db.Column(db.String(2),default="N")
     Rating=db.Column(db.Integer,default=None)
     Comment=db.Column(db.String(1000),default=None)
-
+db.create_all()
 @app.route('/')
 def mainhtml():
     return render_template('mainhtml.html')
